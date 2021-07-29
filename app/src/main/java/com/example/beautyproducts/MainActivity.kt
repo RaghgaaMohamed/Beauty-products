a0a92e916e157fae9c70b35dd2df5ae2923fbc03
Nadia Hassouni <hassouninadia20@gmail.com>
2021-07-29 10:59:23 +0100

    activity main

--- app/src/main/java/com/example/beautyproducts/MainActivity.kt
+++ app/src/main/java/com/example/beautyproducts/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.beautyproducts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.beautyproducts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var imagesList = mutableListOf<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        postToList()
+
+        binding.rvCategories.layoutManager = LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false)
+        binding.rvCategories.adapter = CategoryAdapter(imagesList)
+    }
+
+    private fun postToList() {
+        imagesList.add(R.drawable.flat1)
+        imagesList.add(R.drawable.flat2)
+        imagesList.add(R.drawable.flat3)
+        imagesList.add(R.drawable.flat4)
+        imagesList.add(R.drawable.flat5)
+        imagesList.add(R.drawable.flat6)
+
+
     }
 }
