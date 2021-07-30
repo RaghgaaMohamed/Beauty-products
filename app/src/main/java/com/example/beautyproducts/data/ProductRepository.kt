fcad523b32554be2c12fdbfe034bfbd0f919703c
Nadia Hassouni <hassouninadia20@gmail.com>
2021-07-30 10:56:07 +0100

    repository

--- app/src/main/java/com/example/beautyproducts/data/ProductRepository.kt
+++ app/src/main/java/com/example/beautyproducts/data/ProductRepository.kt
@@ -1,4 +1,13 @@
 package com.example.beautyproducts.data
 
-class ProductRepository {
+import androidx.lifecycle.LiveData
+import com.example.beautyproducts.data.db.ProductDao
+import com.example.beautyproducts.data.db.entities.Product
+
+class ProductRepository(
+    private val productDao: ProductDao
+) {
+    var readAllProducts : LiveData<List<Product>> = productDao.readAllProducts()
+    suspend fun addProduct(product: Product) = productDao.addProduct(product)
+    suspend fun deleteProduct(product: Product) = productDao.deleteProduct(product)
 }
